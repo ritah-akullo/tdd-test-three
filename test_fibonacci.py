03a960d8cb43f596fa7eaf18e91fadb09f2cae76
akullu winnifred <akulluwinnifred1@gmail.com>
2024-07-09 15:27:26 +0300

    third failing test

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -7,5 +7,17 @@ class TestFibonacci(unittest.TestCase):
 
     def test_fibonacci_of_one(self):
         self.assertEqual(fibonacci(1), 1)
+    
+    def test_fibonacci_of_two(self):
+        self.assertEqual(fibonacci(2), 1)
+        
+    def test_fibonacci_of_three(self):
+        self.assertEqual(fibonacci(3), 2)
+        
+    def test_fibonacci_of_four(self):
+        self.assertEqual(fibonacci(4), 3)
+        
+    def test_fibonacci_of_five(self):
+        self.assertEqual(fibonacci(5), 5)
 
 
